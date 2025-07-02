79e5c335b77d62dbb93dfd00ffbda5e6cb530730
Salvador <Salvadoralfaro44@gmail.com>
2025-07-01 21:26:23 -0500

    final auth adjust

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDGBbLMxAERj5AZRcLBy6EXpRIvbai1DOQ",
+  authDomain: "yard-search-5af47.firebaseapp.com",
+  projectId: "yard-search-5af47",
+  storageBucket: "yard-search-5af47.firebasestorage.app",
+  messagingSenderId: "593106097548",
+  appId: "1:593106097548:web:3bc750237c767df67832a9"
 };
 
 // Initialize Firebase
