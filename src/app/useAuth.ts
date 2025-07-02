2e39ae84616e7b5e7b20537a39f35c21abb47e67
Salvador <Salvadoralfaro44@gmail.com>
2025-07-02 02:19:21 -0500

    linting error updated

--- src/app/useAuth.ts
+++ src/app/useAuth.ts
@@ -4,12 +4,9 @@ import { onAuthStateChanged, User } from "firebase/auth";
 
 export function useAuth() {
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      setLoading(false);
     });
 
     return () => unsubscribe();
